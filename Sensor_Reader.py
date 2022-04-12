aa0b6cc02316a1325f363142ad4189f617bfaa54
Tayson Jenkins <taysonjenkins@gmail.com>
2022-04-11 22:04:09 -0700

    save file update

--- Sensor_Reader.py
+++ Sensor_Reader.py
@@ -5,6 +5,9 @@
 import serial
 import time
 
+# expirement counter, tracks the number to make sure we do not overwrite files
+replicate = 1
+
 # set up the serial line
 ser = serial.Serial('COM5', 9600) #('COM4', 9600)
 time.sleep(2)
@@ -29,6 +32,15 @@ ser.close()
 for line in data:
     print(line)
 
+# save data to a txt or csv file
+with open("moisture_data" + str(replicate) + ".txt", "w") as out_file:      #opens a file and writes into the file
+    for i in range(len(data)):                                              # for loop to count through the index of list 
+        out_string = ""                                                     #builds string called "outstring" and is assigned an empty string
+        out_string += str(data[i])                                          #turns 'data' list into a string and uses index 'i' to count
+        out_string += '\n'                                                  # after reading each data point it creates a new line for better visablilty
+        out_file.write(out_string)                                          #writes stringed data to output file 'out_file'
+
+
 #plotting the data
 import matplotlib.pyplot as plt
 # if using a Jupyter notebook include %matplotlib inline
@@ -38,3 +50,10 @@ plt.xlabel('Time (seconds)')
 plt.ylabel('Potentiometer Reading')
 plt.title('Potentiometer Reading vs. Time')
 plt.show()
+
+# save plot to a file
+plt.savefig('moisture_plot' + str(replicate)+'.png')
+
+!git add 'moisture_polt.png'
+!git commit -m "added file1.m"
+!git push
